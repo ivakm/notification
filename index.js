f3cafe34474d1bd6509929a8a5341726a7c4d865
MS <upskxkit3@gmail.com>
2025-05-30 23:32:27 -0300

    add routes for manual update if needed.

--- index.js
+++ index.js
@@ -2,23 +2,70 @@ require('dotenv').config();
 const http = require('http');
 const cron = require('node-cron');
 const { checkNewPosts } = require('./lib/nazk');
-const { generateTextFromArray } = require('./lib/utils');
-
+const { generateTextFromArray, sendResponse } = require('./lib/utils');
+const { HEADERS } = require('./lib/constants');
 const port = 8080;
 let latestParsedData = [];
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  console.log(
-    'Request received, latestParsedDataLength = ',
-    latestParsedData.length,
-  );
-  const responseText = latestParsedData.length
-    ? `Виявлено нові пости:\n
+const routing = {
+  '/': '<h1>welcome to my server</h1>',
+  '/nazk/refresh': async (req, res) => {
+    console.log('refreshing started...');
+    try {
+      latestParsedData = await checkNewPosts();
+      console.log(`Latest parsed data length: ${latestParsedData.length}`);
+    } catch (error) {
+      console.error('Error first nazk scraping:', error);
+    }
+    console.log('refreshing ended');
+
+    sendResponse(res, 200, 'refreshed');
+  },
+  '/nazk/webhook': async (req, res) => {
+    const body = await getBody(req);
+    if (!body) return void sendResponse(res, 400, { message: 'Bad request' });
+
+    console.dir(body);
+
+    try {
+      sendResponse(res, 200, 'OK');
+    } catch (err) {
+      console.error(err);
+      sendResponse(res, 500, { message: 'Internal server error' });
+    }
+  },
+  '/nazk/list': async (req, res) => {
+    const responseText = latestParsedData.length
+      ? `Виявлено нові пости:\n
                         ${generateTextFromArray(latestParsedData)}`
-    : 'Не має нових постів';
-  res.end(responseText);
+      : 'Не має нових постів';
+
+    sendResponse(res, 200, { message: responseText });
+  },
+};
+
+const types = {
+  object: (o) => [200, JSON.stringify(o)],
+  string: (s) => [200, s],
+  undefined: () => [404, 'Page not found'],
+  function: (fn, req, res) => void fn(req, res),
+};
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  for (const [header, value] of HEADERS) res.setHeader(header, value);
+  if (method === 'OPTIONS') return void res.writeHead(204).end();
+
+  const data = routing[url];
+  const type = typeof data;
+  const serializer = types[type];
+  const result = serializer(data, req, res);
+
+  if (!result) return;
+  const [code, response] = result;
+  const contentType = type === 'object' ? 'application/json' : 'text/html';
+  res.writeHead(code, { 'Content-Type': contentType }).end(response);
 });
 
 server.listen(port, '0.0.0.0', async () => {
